c5293a319f8c86fa3a8ce117c00e95337a144c4f
Matt Peters <matthew.george.peters@gmail.com>
2017-05-03 18:21:01 -0400

    finished tweeter.js

--- public/tweeter.js
+++ public/tweeter.js
@@ -1,3 +1,33 @@
-// jQuery for tweeting
+// AJAX request for GETTING data
+$.ajax({
+	type: 'GET',
+	url: '/ajax',
+	success: function (data){
+		for(var x = 0; x < data.tweets.length; x++){
+			appendNewTweet(data.tweets[x]);
+		}
+	}
+});
 
-var 
+//function decloration for posting a new tweet
+function appendNewTweet(tweet){
+	var newTweet = "<div class='tweet-container'>" 
+	+ "<div class='tweet-time'>" + new Date(tweet.time).toLocaleString() +
+	"<div class='tweet-body'>" + tweet.text + "</div>" + "</div>";
+
+	$('#tweet-dest').prepend(newTweet); 
+}
+
+//
+$('#tweet').click(function(){
+	$.ajax({
+		type: 'POST',
+		url: '/ajax',
+		contentType: 'application/json',
+		data: JSON.stringify({tweet: $('new-tweet').val()}),
+		success: function(data){
+			appendNewTweet(data);
+			$('#new-tweet').val('');
+		}
+	})
+});
